bc01306985848ad0b6933ff0ae6894b1c77f08f9
IvanRamyk <ivan.ramyk.official@gmail.com>
2020-11-28 22:57:58 +0200

    small fixes

--- src/main/java/AbstractFixnumLock.java
+++ src/main/java/AbstractFixnumLock.java
@@ -5,29 +5,28 @@ import java.util.List;
 
 public abstract class AbstractFixnumLock implements FixnumLock{
     private int numberOfThreads;
-    private List<AbstractMap.SimpleEntry<Long, Integer>> registeredThreads;
-    private List<Integer> freeID;
+    private int cntFreeThreads;
+    private List<Long> registeredThreads;
+    private List<Integer> freeIDs;
 
     private static final Object syncObj = new Object();
 
     AbstractFixnumLock(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
+        this.cntFreeThreads = numberOfThreads;
         this.registeredThreads = new ArrayList<>();
-        initializeFreeID();
-    }
-
-    private void initializeFreeID() {
-        freeID = new ArrayList<>();
+        this.freeIDs = new ArrayList<>();
         for (int i = 0; i < numberOfThreads; i++) {
-            this.freeID.add(i);
+            this.freeIDs.add(i);
+            this.registeredThreads.add((long) -1);
         }
     }
 
     @Override
     public int getId() {
-        for (AbstractMap.SimpleEntry<Long, Integer> item : registeredThreads) {
-            if (item.getKey() == Thread.currentThread().getId()) {
-                return item.getValue();
+        for (int i = 0; i < numberOfThreads; ++i) {
+            if (this.registeredThreads.get(i) == Thread.currentThread().getId()) {
+                return i;
             }
         }
         return -1;
@@ -37,19 +36,14 @@ public abstract class AbstractFixnumLock implements FixnumLock{
     public int register() {
         synchronized (syncObj) {
             int id = getId();
-            if (!freeID.isEmpty()) {
-                if (id != -1) {
-//                    System.out.println("Thread with id " + id + " already registered.");
-                } else {
-                    registeredThreads.add(new AbstractMap.SimpleEntry<>(Thread.currentThread().getId(), freeID.get(0)));
-                    freeID.remove(freeID.get(0));
-//                    System.out.println("Thread with id " + getId() + " is registered.");
-                    return getId();
-                }
-            } else {
-//                System.out.println("Threads number limit already reached. Can't register thread with actual id " + Thread.currentThread().getId());
+            if (id == - 1) {
+                if (cntFreeThreads == 0)
+                    return -1;
+                cntFreeThreads--;
+                registeredThreads.set(freeIDs.get(0), Thread.currentThread().getId());
+                freeIDs.remove(freeIDs.get(0));
             }
-            return -1;
+            return getId();
         }
     }
 
@@ -58,8 +52,8 @@ public abstract class AbstractFixnumLock implements FixnumLock{
         synchronized (syncObj) {
             int id = getId();
             if (id != -1) {
-                registeredThreads.remove(new AbstractMap.SimpleEntry<>(Thread.currentThread().getId(), id));
-                freeID.add(id);
+                registeredThreads.set(id, (long) -1);
+                freeIDs.add(id);
 //                System.out.println("Unregister thread with id " + id + ".");
                 return id;
             } else {
@@ -72,7 +66,12 @@ public abstract class AbstractFixnumLock implements FixnumLock{
     private void reset() {
         synchronized (syncObj) {
             registeredThreads.clear();
-            initializeFreeID();
+            this.cntFreeThreads = numberOfThreads;
+            this.freeIDs.clear();
+            for (int i = 0; i < numberOfThreads; i++) {
+                this.freeIDs.add(i);
+                this.registeredThreads.add((long) -1);
+            }
         }
     }
 }
